c39e5f641cba0f1df757594bbeda2fabdb72cfe5
David <davidcondemorales@gmail.com>
2022-03-23 12:21:44 +0100

    Updated options description

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # cloud2mesh
-Converting point clouds to textured meshes
+Converting point clouds to textured meshes.
+
+## Use
+Open [`options.py`](options.py) and assign to `FILENAME` the path to your point cloud.
+
+Enabling `SPLIT` will produce as many clouds as specified in `GRID_SIZE`, with the option to set their pivot as the center of the original cloud if enabling `CENTER_CLOUD`.
+
+Enabling `PROCESS` will produce the meshes of all the clouds in the "clouds/" directory. By setting a path to a cloud in `SINGLE_FILENAME` only that file will be processed.
+
+After adjusting settings, run the [`main.py`](main.py) script. Outputs are stored in individual folders in the "clouds/" directory.
--- options.py
+++ options.py
@@ -1,19 +1,19 @@
 FILENAME = 'input/Cloud.txt'
-DEBUG = True
+DEBUG = False
 
 # Tasks
 SPLIT = True
 PROCESS = True
 
 # Input settings
-RGB_MODE = 0  # 0 -> [0-255]; # 1 -> [0.0-1.0]
+RGB_MODE = 0  # range of color: 0 -> [0-255]; # 1 -> [0.0-1.0]
 DATA_FORMAT = 'X Y Z R G B Reflectance'  # default: 'X Y Z R G B Reflectance'
 
 # Splitting options
-CENTER_CLOUD = True
-GRID_SIZE = [4, 4, 1]
+CENTER_CLOUD = True  # set local origin at the center of the cloud
+GRID_SIZE = [4, 4, 1]  # number of splits per axis
 
 # Processing options
 SINGLE_FILENAME = None  # if 'None', process all files in 'clouds' folder
-TEXTDIM = 4096
-SIMPLIF_PRCNT = 100
+TEXTDIM = 4096  # resolution of the texture
+SIMPLIF_PRCNT = 100  # simplification percentage of each cloud split
